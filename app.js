c80fc4d8388148a0685a3ffbca760188eeaf2ee3
hks_74123 <56163188+hks74123@users.noreply.github.com>
2021-05-22 21:49:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+jj=0;
+
+function showoption() {
+    jj=jj+1;
+    if(jj%2!=0){
+    document.getElementById("dropc").style.display="block";
+    }
+    else{
+        document.getElementById("dropc").style.display="none";
+    }
+}
